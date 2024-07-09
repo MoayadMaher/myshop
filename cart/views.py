e6997fd04c307b80b87b3cc7709a47e3babeb2b5
MoayadMaher <its.moayadj@gmail.com>
2024-07-09 12:27:08 +0300

    WIP

--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render , redirect , get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
+
 
 @require_POST
 def cart_add(request, product_id):
@@ -11,8 +12,11 @@ def cart_add(request, product_id):
     form = CartAddProductForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
-        cart.add(product=product,quantity=cd['quantity'],override_quantity=cd['override'])
+        cart.add(product=product,
+                 quantity=cd['quantity'],
+                 override_quantity=cd['override'])
     return redirect('cart:cart_detail')
+
 
 @require_POST
 def cart_remove(request, product_id):
@@ -21,6 +25,11 @@ def cart_remove(request, product_id):
     cart.remove(product)
     return redirect('cart:cart_detail')
 
+
 def cart_detail(request):
     cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(initial={
+                            'quantity': item['quantity'],
+                            'override': True})
     return render(request, 'cart/detail.html', {'cart': cart})
